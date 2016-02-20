2be7858f4fbc8d1fb0d51a5e56471247a55ee00e
Shafiqul-Nasrul <shafiqulislam561@gmail.com>
2016-02-20 13:20:47 +0600

    Added files via upload

---
+++ login.java
@@ -0,0 +1,95 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTextField;
+
+public class login {
+	String username;
+	String password;
+
+   
+	private JFrame frame;
+    private JTextField textField;
+    private JTextField textField_1;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					login window = new login();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public login() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JButton btnLogin = new JButton("LOGIN");
+		btnLogin.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				System.out.println(username=textField.getText());
+				System.out.println(password=textField_1.getText());
+		    JOptionPane.showMessageDialog(null, "Login Successful");
+		    frame.dispose();
+			StudentReg stdreg = new StudentReg();
+			stdreg.setVisible(true);
+			}
+		   
+		
+		});
+		
+		
+		
+		
+		btnLogin.setBounds(175, 182, 89, 23);
+		frame.getContentPane().add(btnLogin);
+		
+		
+		
+		JLabel lblUserName = new JLabel("User Name");
+		lblUserName.setFont(new Font("Tahoma", Font.BOLD, 13));
+		lblUserName.setBounds(31, 85, 86, 23);
+		frame.getContentPane().add(lblUserName);
+		
+		JLabel lblNewLabel = new JLabel("Password:");
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 13));
+		lblNewLabel.setBounds(31, 122, 77, 19);
+		frame.getContentPane().add(lblNewLabel);
+		
+		textField = new JTextField();
+		textField.setBounds(165, 87, 178, 20);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(165, 122, 178, 20);
+		frame.getContentPane().add(textField_1);
+		textField_1.setColumns(10);
+	}
+}
